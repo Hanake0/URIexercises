d28b7ab99a2245292cf9d68a01e7f91bad56f9a5
Hanake <67035049+Hanake0@users.noreply.github.com>
2021-05-25 18:48:16 +0000

    Refactor E

--- Java/ex1002.java
+++ Java/ex1002.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
+	static final double pi = 3.14159;
+
+	public static void main(String[] args) {
  
-    public static void main(String[] args) {
- 
-        double pi = 3.14159;
-        
-        Scanner in = new Scanner(System.in);
-        double r = in.nextDouble();
-        in.close();
-        
-        System.out.printf("A=%.4f/n", pi*r*r);
-    }
- 
+		Scanner in = new Scanner(System.in);
+		double r = in.nextDouble();
+		in.close();
+
+		System.out.printf("A=%.4f\n", pi*r*r);
+	}
 }
