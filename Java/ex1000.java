5dfca99858e3d1ad8ecf3599ed0eaa6a7492b9bb
Hanake <67035049+Hanake0@users.noreply.github.com>
2021-05-25 19:53:56 +0000

    Refactor ex1000

--- Java/ex1000.java
+++ Java/ex1000.java
@@ -1,10 +1,7 @@
-import java.io.IOException;
-
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
- 
-        System.out.println("Hello World!");
-    }
- 
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
 }
