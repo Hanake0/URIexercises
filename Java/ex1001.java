55c9e2c9aadd52fda02795ea78096dd3c3af9a56
Hanake <67035049+Hanake0@users.noreply.github.com>
2021-05-25 19:56:53 +0000

    Refactor ex1001

--- Java/ex1001.java
+++ Java/ex1001.java
@@ -1,17 +1,15 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
- 
-        Scanner in = new Scanner(System.in);
-        
-        int a = in.nextInt();
-        int b = in.nextInt();
-        in.close();
-        
-        System.out.println("X = " + (a + b));
-    }
- 
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int a = in.nextInt();
+		int b = in.nextInt();
+		in.close();
+
+		System.out.println("X = " + (a + b));
+	}
 }
