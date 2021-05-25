65355073aa2bd27bb577d0c2e9064c843bb6a5a3
Hanake <67035049+Hanake0@users.noreply.github.com>
2021-05-25 20:15:40 +0000

    Refactor ex1005

--- Java/ex1005.java
+++ Java/ex1005.java
@@ -1,20 +1,14 @@
-import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
  
-    public static void main(String[] args) throws IOException {
- 
-        Scanner in = new Scanner(System.in);
-        float a = in.nextFloat();
-        float b = in.nextFloat();
-        in.close();
-        
-        DecimalFormat format = new DecimalFormat("#.#####");
-        format.setMinimumFractionDigits(5);
-        
-        System.out.println("MEDIA = " + format.format(((a*3.5f) + (b*7.5f))/11f));
-    }
- 
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		float a = in.nextFloat();
+		float b = in.nextFloat();
+		in.close();
+
+		System.out.printf("MEDIA = %.5f", ( ((a*3.5f) + (b*7.5f))/11f ));
+	}
 }
