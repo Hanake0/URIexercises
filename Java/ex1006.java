5c35af1fdbec10b843507eab4dc27038ac806090
Hanake <67035049+Hanake0@users.noreply.github.com>
2021-05-25 20:23:44 +0000

    Refactor ex1006

--- Java/ex1006.java
+++ Java/ex1006.java
@@ -1,21 +1,15 @@
-import java.io.IOException;
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
- 
-        Scanner in = new Scanner(System.in);
-        float a = in.nextFloat();
-        float b = in.nextFloat();
-        float c = in.nextFloat();
-        in.close();
-        
-        DecimalFormat format = new DecimalFormat("#.#");
-        format.setMinimumFractionDigits(1);
-        
-        System.out.println("MEDIA = " + format.format(((a*2f) + (b*3f) + (c*5f))/10f));
-    }
- 
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		float a = in.nextFloat();
+		float b = in.nextFloat();
+		float c = in.nextFloat();
+		in.close();
+
+		System.out.printf("MEDIA = %.1f", ( ((a*2f) + (b*3f) + (c*5f))/10f ));
+	}
 }
