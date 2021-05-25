c0b5e205f2851fd7f358880d70f1cd25bf1e6b5c
Hanake <67035049+Hanake0@users.noreply.github.com>
2021-05-25 20:10:51 +0000

    Refactor ex1004

--- Java/ex1004.java
+++ Java/ex1004.java
@@ -1,16 +1,14 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
  
-    public static void main(String[] args) throws IOException {
- 
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        in.close();
-        
-        System.out.println("PROD = " + (a * b));
-    }
- 
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		in.close();
+
+		System.out.printf("PROD = %d", (a * b));
+	}
 }
