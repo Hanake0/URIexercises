2746a32e152a679557929487c472d678522a4cea
Hanake <67035049+Hanake0@users.noreply.github.com>
2021-05-25 20:35:39 +0000

    Refactor ex1060

--- Java/ex1060.java
+++ Java/ex1060.java
@@ -1,26 +1,19 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
- 
-        Scanner in = new Scanner(System.in);
-        
-        int positives = 0;
-        for(int i = 0; i<=6 ; i++) {
-            float num = -1;
-            if(in.hasNextFloat())
-                num = in.nextFloat();
-            
-            if (num == Math.abs(num))
-               	positives++;
-            
-        }
-        
-        in.close();
-        
-        System.out.println(positives + " valores positivos");
-    }
- 
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int positives = 0;
+		for(int i = 0; i<=6 ; i++) {
+			float num = in.hasNextFloat() ? in.nextFloat() : -1;
+			if (num == Math.abs(num))
+				positives++;
+		}
+		in.close();
+
+		System.out.printf("%d valores positivos\n", positives);
+	}
 }
