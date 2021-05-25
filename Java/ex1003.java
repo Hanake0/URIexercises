f3b5e8e2910cac4ddd98866eb811ca826e0cf8b0
Hanake <67035049+Hanake0@users.noreply.github.com>
2021-05-25 20:07:51 +0000

    Refactor ex1003

--- Java/ex1003.java
+++ Java/ex1003.java
@@ -1,16 +1,14 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
- 
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        in.close();
-        
-        System.out.println("SOMA = " + (a + b));
-    }
- 
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int a = in.nextInt();
+		int b = in.nextInt();
+		in.close();
+
+		System.out.printf("SOMA = %d", (a + b));
+	}
 }
